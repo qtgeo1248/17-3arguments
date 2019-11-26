65b5fe9a65752659ada2597c78dc85ffd97698b5
qtgeo1248 <gzhou00@stuy.edu>
2019-11-26 15:04:12 -0500

    starter

---
+++ args.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
