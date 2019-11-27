9a24ba9ed6a3f161e0e69297a3f204f8af1e8dcb
qtgeo1248 <gzhou00@stuy.edu>
2019-11-26 21:52:30 -0500

    did it!

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 int main(int argc, char *arbv[]) {
     char *test;
     char buf[1000];
-    printf("Please input a program:\n");
+    printf("Please input a program (for example, \"ls -l -a\"):\n");
     fgets(buf, 1000, stdin);
     buf[strlen(buf) - 1] = 0;
     test = buf;
     char **args = parse_args(test);
     execvp(args[0], args);
+
+    int i = 0;
+    for (; args[i] != NULL; i++) {
+        free(args[i]);
+    }
     return 0;
 }
